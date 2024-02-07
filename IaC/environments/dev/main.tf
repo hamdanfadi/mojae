9d553d7a8cac637be9ca870013ade8662c7f8636
Amr <amr_ashraf_mohamed_amin@yahoo.com>
2024-02-07 08:56:08 +0200

    Add the skiliton of the IaC for TMC

---
+++ IaC/environments/dev/main.tf
@@ -0,0 +1,9 @@
+# https://registry.terraform.io/providers/vmware/tanzu-mission-control/latest/docs
+# https://registry.terraform.io/providers/vmware/tanzu-mission-control/latest/docs/guides/tanzu-mission-control_cluster
+# https://registry.terraform.io/providers/vmware/tanzu-mission-control/latest/docs/resources/cluster
+
+
+# You must also have the appropriate permissions:
+
+# To provision a cluster, you must have admin permissions on the management cluster to provision resources within it.
+# You must also have clustergroup.edit permissions on the cluster group in which you want to put the new cluster.
---
+++ IaC/environments/dev/provider.tf
@@ -0,0 +1,23 @@
+# https://registry.terraform.io/providers/vmware/tanzu-mission-control/latest/docs
+
+terraform {
+  required_providers {
+    tanzu-mission-control = {
+      source = "vmware/tanzu-mission-control"
+      version = "1.4.2"
+    }
+  }
+}
+
+
+# Provider configuration for TMC Self-Managed
+provider "tanzu-mission-control" {
+  endpoint = var.endpoint # optionally use TMC_ENDPOINT env var
+
+  self_managed {
+    oidc_issuer = var.oidc_issuer # optionally use OIDC_ISSUER env var,  Ex: export OIDC_ISSUER=pinniped-supervisor.example.local-dev.tmc.com
+    username    = var.username    # optionally use TMC_SM_USERNAME env var
+    password    = var.password    # optionally use TMC_SM_PASSWORD env var
+  }
+  ca_file = var.ca_file # Path to Host's root ca set. The certificates issued by the issuer should be trusted by the host accessing TMC Self-Managed via TMC terraform provider.
+}
