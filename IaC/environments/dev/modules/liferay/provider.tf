0ad34a4f21ec2e40cee9073fc9928943bdca7579
Maria <Mss10047@students.aust.edu.lb>
2024-02-09 10:14:13 +0200

    adding VM for liferay db

--- IaC/environments/dev/modules/liferay/provider.tf
+++ IaC/environments/dev/modules/liferay/provider.tf
@@ -17,12 +17,12 @@ terraform {
 
 # Provider configuration for TMC Self-Managed
 provider "tanzu-mission-control" {
-  endpoint = var.endpoint # optionally use TMC_ENDPOINT env var
+  endpoint = var.tmc_endpoint # optionally use TMC_ENDPOINT env var
 
   self_managed {
-    oidc_issuer = var.oidc_issuer # optionally use OIDC_ISSUER env var,  Ex: export OIDC_ISSUER=pinniped-supervisor.example.local-dev.tmc.com
-    username    = var.username    # optionally use TMC_SM_USERNAME env var
-    password    = var.password    # optionally use TMC_SM_PASSWORD env var
+    oidc_issuer = var.tmc_oidc_issuer # optionally use OIDC_ISSUER env var,  Ex: export OIDC_ISSUER=pinniped-supervisor.example.local-dev.tmc.com
+    username    = var.tmc_username    # optionally use TMC_SM_USERNAME env var
+    password    = var.tmc_password    # optionally use TMC_SM_PASSWORD env var
   }
   ca_file = var.ca_file # Path to Host's root ca set. The certificates issued by the issuer should be trusted by the host accessing TMC Self-Managed via TMC terraform provider.
 }
@@ -38,9 +38,10 @@ provider "vcd" {
   url                  = var.vcd_url
   max_retry_timeout    = var.vcd_max_retry_timeout
   allow_unverified_ssl = var.vcd_allow_unverified_ssl
-}
+} 
  
 # Create a new network in organization and VDC defined above
 resource "vcd_network_routed" "net" {
-  # ...
+  name = ""
+  edge_gateway = ""
 }
